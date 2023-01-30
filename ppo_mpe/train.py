bf0046233bc72455f8fac268903edd2a773f1165
Nikunj Gupta <guptanikunj1997@gmail.com>
2023-01-30 16:15:41 -0500

    logging tensorboard; added argparse;

--- ppo_mpe/train.py
+++ ppo_mpe/train.py
@@ -2,15 +2,27 @@ from pettingzoo.mpe import simple_spread_v2
 from ppo_mpe import PPO 
 from random_policy import RandomPolicy 
 from torch.utils.tensorboard import SummaryWriter 
+import torch, numpy as np, argparse, os 
 
+
+parser = argparse.ArgumentParser() 
+parser.add_argument("--seed", type=int) 
+parser.add_argument("--n_agents", type=int)
+parser.add_argument("--log_dir", type=str)
+args = parser.parse_known_args()[0] 
+
+log_name = "noam"  
+log_name.append("n_" + str(args.n_agents)) 
+log_name.append("seed_" + str(args.seed)) 
+log_name = "--".join(log_name) 
 
 hyperparams = {
-    "n_agents": 2, 
+    "n_agents": args.n_agents, 
     "max_episodes":30_000,
     "max_cycles":25,
     "update_timestep": 30, 
     "save_model_freq": 5_000, 
-    "logs_dir": "ppo_mpe/logs/test", 
+    "logs_dir": os.path.join(args.log_dir, log_name), 
     "action_std_decay_rate": 0.05, 
     "action_std_decay_freq": int(2.5e5), 
     "min_action_std": 0.1, 
@@ -28,6 +40,10 @@ env = simple_spread_v2.parallel_env(
     max_cycles=hyperparams["max_cycles"],
     continuous_actions=hyperparams["has_continuous_action_space"]
 )
+# seed
+np.random.seed(args.seed)
+torch.manual_seed(args.seed)
+env.reset(seed=args.seed) 
 
 state_dim = env.observation_space(env.possible_agents[0]).shape[0]
 action_dim = env.action_space(env.possible_agents[0]).shape[0] if hyperparams["has_continuous_action_space"] else env.action_space(env.possible_agents[0]).n 
@@ -48,7 +64,7 @@ for i_episode in range(1, hyperparams["max_episodes"]+1):
     for t in range(1, hyperparams["max_cycles"]+1):
         action = {}
         for a in range(hyperparams["n_agents"]): 
-            action[env.possible_agents[a]] = agents[a].select_action(state[env.possible_agents[a]])  
+            action[env.possible_agents[a]] = agents[a].select_action(state[env.possible_agents[a]]) 
         state, reward, done, is_terminals, info = env.step(action) 
 
         # saving reward and is_terminals
@@ -64,9 +80,6 @@ for i_episode in range(1, hyperparams["max_episodes"]+1):
                 if isinstance(agents[a], PPO): 
                     agents[a].decay_action_std(hyperparams["action_std_decay_rate"], hyperparams["min_action_std"]) 
         
-        if t % 10_000 == 0: 
-            print("Episode : {} \t\t Timestep : {} \t\t Average Reward : {}".format(i_episode, t, ep_reward)) 
-
         if any(list(done.values())): 
             break 
             
