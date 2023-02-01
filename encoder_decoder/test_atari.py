467bb5f7b192d1ea6124b093e9207fdde060c860
Nikunj Gupta <guptanikunj1997@gmail.com>
2023-02-01 16:17:31 -0500

    now logging tensorboard summaries

--- encoder_decoder/test_atari.py
+++ encoder_decoder/test_atari.py
@@ -1,8 +1,14 @@
 from pettingzoo.atari import maze_craze_v3  
 from supersuit import color_reduction_v0, frame_stack_v1, resize_v1 
+
 from torch.utils.tensorboard import SummaryWriter 
-import torch, numpy as np, argparse, os 
+import torch, numpy as np, argparse, os
 import torch.optim as optim 
+from torch.utils.tensorboard import SummaryWriter
+
+from itertools import count
+from pathlib import Path 
+
 from vae import VAE 
 from random_policy import RandomPolicy 
 
@@ -10,7 +16,6 @@ from random_policy import RandomPolicy
 Hyperparams 
 """ 
 # multi-agent atari 
-
 frame_size = (64, 64)
 has_continuous_action_space = False 
 seed = 10 
@@ -23,10 +28,21 @@ kld_weight = 0.00025
 
 # RL 
 n_agents = 2 
-max_episodes = 2_000 
+max_episodes = 10_000 
 max_timesteps = 100_000 
-save_timestep = 1_000  
+save_timestep = 5_000 
+log_dir = "encoder_decoder/logs/"
 
+# writer = SummaryWriter(log_dir)
+log_dir = Path(log_dir)
+for i in count(0):
+    temp = log_dir/('run{}'.format(i)) 
+    if temp.exists():
+        pass
+    else:
+        writer = SummaryWriter(temp)
+        log_dir = temp
+        break
 # set device 
 if torch.cuda.is_available(): device = torch.device('cuda')
 else: device = torch.device('cpu')
@@ -61,10 +77,13 @@ agents = [
 vae = VAE(in_channels=in_channels, latent_dim=latent_size, hidden_dims=None).to(device)
 
 optimizer = optim.Adam(vae.parameters(), lr=learning_rate)
+global_timestep = 0 
 for episode in range(1, max_episodes+1): 
     state = env.reset() 
     inputs = agents[0].batchify_obs(state, device).detach().numpy() 
+    losses, recon_losses, KL_losses = [], [], [] 
     for timestep in range(1, max_timesteps+1): 
+        global_timestep+=1 
         action = {}
         for a in range(n_agents): 
             action[env.possible_agents[a]] = agents[a].select_action(agents[a].batchify_obs(state, device))  
@@ -79,6 +98,18 @@ for episode in range(1, max_episodes+1):
             optimizer.step()
             l = loss.item() 
             inputs = agents[0].batchify_obs(state, device).detach().numpy() 
-            torch.save(vae, "encoder_decoder/maze_models/vae_temp.pt")
+        
+            writer.add_scalar("1000-Timestep/loss", l, global_timestep) 
+            writer.add_scalar("1000-Timestep/reconstruction loss", recon_loss.item(), global_timestep) 
+            writer.add_scalar("1000-Timestep/KL", kl.item(), global_timestep) 
+
+            losses.append(l)
+            recon_losses.append(recon_loss.item()) 
+            KL_losses.append(kl.item()) 
+        
+            torch.save(vae, log_dir/('vae.pt') )
             print("Episode: {}, Timestep: {}, Loss: {}, Reconstruction loss: {}, KL: {}".format(episode, timestep, l, recon_loss.item(), kl.item())) 
+    writer.add_scalar("Episode/loss", np.mean(losses), episode) 
+    writer.add_scalar("Episode/reconstruction loss", np.mean(recon_losses), episode) 
+    writer.add_scalar("Episode/KL", np.mean(KL_losses), episode) 
 
