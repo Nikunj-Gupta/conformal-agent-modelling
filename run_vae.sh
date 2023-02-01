f0adfeec31022cdb849ae0f4588ca258e22c5195
Nikunj Gupta <guptanikunj1997@gmail.com>
2023-02-01 16:19:00 -0500

    script to run on compute canada

---
+++ run_vae.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+#SBATCH --account=rrg-ebrahimi
+#SBATCH --mem=32G       
+#SBATCH --cpus-per-task=6
+#SBATCH --time=72:00:00
+#SBATCH --gres=gpu:v100:1
+#SBATCH --output=atari-vae-run.out
+
+source venv/bin/activate
+
+tensorboard --logdir=encoder_decoder/logs --host 0.0.0.0 --load_fast false &
+time python encoder_decoder/test_atari.py > log1.out 
