5ded4961a442c11d2dbf9d6997eb0894229cd732
Nikunj-Gupta <ng2531@nyu.edu>
2023-02-07 02:24:50 -0500

    runs

--- run_noam_random.sh
+++ run_noam_random.sh
@@ -3,13 +3,11 @@
 #SBATCH --time=00:30:00
 #SBATCH --account=rrg-ebrahimi
 #SBATCH --mem=32G
-#SBATCH --output=out/%x_%A.out
-#SBATCH --error=out/%x_%A.err
 #SBATCH --cpus-per-task=4
-#SBATCH --job-name=mpe_noam_random
 #SBATCH --array=0-20
 
 source venv/bin/activate
+module load python/3.10
 export OMP_NUM_THREADS=1 #init weights fails otherwise (see https://github.com/pytorch/pytorch/issues/21956)
 runs=${SLURM_ARRAY_TASK_ID}
 
