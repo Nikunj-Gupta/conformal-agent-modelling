d1897374bc7a31f49e35ceca152b2ec8f5cdea0a
Nikunj-Gupta <ng2531@nyu.edu>
2023-02-06 20:05:01 -0500

    added all runs

--- run_noam_marl.sh
+++ run_noam_marl.sh
@@ -6,11 +6,11 @@
 #SBATCH --output=out/%x_%A.out
 #SBATCH --error=out/%x_%A.err
 #SBATCH --cpus-per-task=4
-#SBATCH --job-name=mpe_giam
-#SBATCH --array=0-20
+#SBATCH --job-name=mpe_noam_marl
+#SBATCH --array=0-10
 
 source venv/bin/activate
 export OMP_NUM_THREADS=1 #init weights fails otherwise (see https://github.com/pytorch/pytorch/issues/21956)
 runs=${SLURM_ARRAY_TASK_ID}
 
-python giam/giam.py --n_agents=2 --log_dir=mpe_logs --seed=$runs 
+python ppo_mpe/train.py --n_agents=2 --random_other_agent=1 --log_dir=mpe_logs --seed=$runs 
--- run_noam_random.sh
+++ run_noam_random.sh
@@ -6,8 +6,8 @@
 #SBATCH --output=out/%x_%A.out
 #SBATCH --error=out/%x_%A.err
 #SBATCH --cpus-per-task=4
-#SBATCH --job-name=mpe
-#SBATCH --array=0-10
+#SBATCH --job-name=mpe_noam_random
+#SBATCH --array=0-20
 
 source venv/bin/activate
 export OMP_NUM_THREADS=1 #init weights fails otherwise (see https://github.com/pytorch/pytorch/issues/21956)
