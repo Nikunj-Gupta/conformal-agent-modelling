e59932ec11d021ba50ea242773d1ea246e86c1a5
Nikunj-Gupta <ng2531@nyu.edu>
2023-02-09 18:53:13 -0500

    added more runs

---
+++ run_cam2.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+#SBATCH --time=20:00:00
+#SBATCH --account=rrg-ebrahimi
+#SBATCH --mem=64G
+#SBATCH --output=out/%x_%A.out
+#SBATCH --error=out/%x_%A.err
+#SBATCH --cpus-per-task=6
+#SBATCH --array=0-20
+
+source venv/bin/activate
+export OMP_NUM_THREADS=1 #init weights fails otherwise (see https://github.com/pytorch/pytorch/issues/21956)
+runs=${SLURM_ARRAY_TASK_ID}
+
+python conformal-action-prediction/conformal-rl.py --n_agents=2 --log_dir=cp_mpe_logs --seed=$runs --cp_update_timestep 30 
