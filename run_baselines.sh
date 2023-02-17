ed779fa1072fd612d75dde484484a9377ce6d1b6
Nikunj-Gupta <ng2531@nyu.edu>
2023-02-17 13:11:07 -0500

    added script to run exps for 4 envs for giam and noam

---
+++ run_baselines.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+
+#SBATCH --time=20:00:00
+#SBATCH --account=rrg-ebrahimi
+#SBATCH --mem=64G
+#SBATCH --output=out/%x_%A.out
+#SBATCH --error=out/%x_%A.err
+#SBATCH --cpus-per-task=6
+#SBATCH --array=0-20
+
+
+source venv/bin/activate
+
+export OMP_NUM_THREADS=1 #init weights fails otherwise (see https://github.com/pytorch/pytorch/issues/21956)
+env=${1}
+baseline=${2}
+
+runs=${SLURM_ARRAY_TASK_ID}
+
+time python baselines/baselines.py --envname=$env --baseline=$baseline --log_dir=env-search --seed=$runs 
