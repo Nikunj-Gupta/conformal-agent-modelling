28894f0a2e654345953e7136913f0b125326466a
Nikunj-Gupta <ng2531@nyu.edu>
2023-02-20 12:36:00 -0500

    added more runs for cp-representations

--- run_all_cp.sh
+++ run_all_cp.sh
@@ -1,4 +1,5 @@
-list="5 10 15 20 25 30 35 40 45 50"
+# list="5 10 15 20 25 30 35 40 45 50"
+list="60 70 80 90 100 110 120 130 140 150 175 200"
 for cp_update_timestep in $list
 do
     mkdir -p out/
