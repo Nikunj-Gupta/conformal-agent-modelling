38472b10970b391768c1b1161ed52149ef7f10bd
Nikunj-Gupta <ng2531@nyu.edu>
2023-02-27 13:36:54 -0500

    compute canada run scripts

---
+++ scripts/run_all_baselines.sh
@@ -0,0 +1,17 @@
+# envs="simple_spread_v2 simple_push_v2 simple_speaker_listener_v3 simple_world_comm_v2"
+# envs="simple_spread_v2 simple_world_comm_v2 simple_adversary_v2 simple_tag_v2" 
+envs="simple_tag_v2" 
+baselines="giam noam"
+for env in $envs
+do
+    for baseline in $baselines
+        do
+            mkdir -p out/
+            run_cmd="run_baselines.sh ${env} ${baseline}"
+            sbatch_cmd="sbatch ${run_cmd}"
+            cmd="$sbatch_cmd"
+            echo -e "${cmd}"
+            ${cmd}
+            sleep 1
+        done
+done
---
+++ scripts/run_baselines.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+
+#SBATCH --time=4:00:00
+#SBATCH --account=rrg-ebrahimi
+#SBATCH --mem=64G
+#SBATCH --output=out/%x_%A.out
+#SBATCH --error=out/%x_%A.err
+#SBATCH --cpus-per-task=6
+#SBATCH --array=0-20
+
+
+source venv/bin/activate
+
+export OMP_NUM_THREADS=1 #init weights fails otherwise (see https://github.com/pytorch/pytorch/issues/21956)
+env=${1}
+baseline=${2}
+
+runs=${SLURM_ARRAY_TASK_ID}
+
+time python ../baselines/baselines.py --envname=$env --baseline=$baseline --log_dir="debug_logs/simple-tag-1_adv-2_obs/" --seed=$runs 
