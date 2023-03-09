d360e026a273d068c1d5deb0548428ec56ce26ca
Nikunj-Gupta <ng2531@nyu.edu>
2023-03-09 18:44:07 -0500

    change env config

--- scripts/run_lbf.sh
+++ scripts/run_lbf.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 
-#SBATCH --time=4:00:00
+#SBATCH --time=10:00:00
 #SBATCH --account=def-ebrahimi
-#SBATCH --mem=128G
+#SBATCH --mem=64G
 #SBATCH --output=out/%x_%A.out
 #SBATCH --error=out/%x_%A.err
 #SBATCH --cpus-per-task=6
@@ -18,6 +18,7 @@ runs=${SLURM_ARRAY_TASK_ID}
 
 tensorboard --logdir="./debug_logs/lbf" --host 0.0.0.0 --load_fast false & 
 time python baselines/baselines_lbf.py \
+                    --env="Foraging-12x12-2p-4f-v1" \
                     --baseline=$baseline \
                     --log_dir="./debug_logs/lbf/" \
                     --seed=$runs
